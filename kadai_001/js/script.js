e9e901d1f471cc4829423d0d32864336cdb2d30e
minami0018 <minami0018@icloud.com>
2025-07-05 09:50:11 +0900

    Add files via upload

---
+++ kadai_001/js/script.js
@@ -0,0 +1,74 @@
+$(function () {
+ 
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    fade: true,
+    speed: 1500,
+    pauseOnHover: false
+  });
+
+  // リンクのホバー時
+  $('a, .item img').hover(
+    function () {
+      $(this).animate({ 'opacity': 0.6 }, 300);
+    },
+    function () {
+      $(this).animate({ 'opacity': 1.0 }, 300);
+    }
+  );
+
+  // TOPに戻るボタン 
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#page-top').fadeIn();
+    } else {
+      $('#page-top').fadeOut();
+    }
+  });
+
+  // ページ内リンクのスクロール
+  $('a[href^="#"]').click(function () {
+    const speed = 500;
+    const href = $(this).attr('href');
+    let $target;
+    if (href == '#') {
+      $target = $('html');
+    }
+    else {
+      $target = $(href);
+    }
+    const position = $target.offset().top;
+    $('html, body').animate({ 'scrollTop': position }, speed, 'swing');
+    return false;
+  });
+
+  // スクロールしたときにフェードイン
+  $(window).scroll(function () {
+    const scrollAmount = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    $('section').each(function () {
+      const position = $(this).offset().top;
+      if (scrollAmount > position - windowHeight + 100) {
+        $(this).addClass('fade-in');
+      }
+    });
+  });
+
+  // Worksの画像をクリックしたとき拡大表示
+  $('.works img').click(function () {
+    const imgSrc = $(this).attr('src');
+    const imgAlt = $(this).attr('alt');
+    $('.big-img').attr({
+      src: imgSrc,
+      alt: imgAlt,
+    });
+    $('.modal').fadeIn();
+  });
+
+  // 閉じるボタンでモーダルを閉じる
+  $('.close-btn').click(function () {
+    $('.modal').fadeOut();
+  });
+});
